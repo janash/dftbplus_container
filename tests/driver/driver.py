479c459642b7618c4d7e08eea2084807f06030f7
Jessica Nash <janash@vt.edu>
2022-06-20 16:55:26 +0000

    most recent driver and functionality report'

--- tests/driver/driver.py
+++ tests/driver/driver.py
@@ -2,6 +2,8 @@ import mdi
 from mdi import MDI_NAME_LENGTH, MDI_COMMAND_LENGTH
 import sys
 
+import numpy as np
+
 iarg = 1
 while iarg < len(sys.argv):
     arg = sys.argv[iarg]
@@ -23,8 +25,45 @@ comm = mdi.MDI_Accept_Communicator()
 # Get the name of the engine
 mdi.MDI_Send_Command("<NAME", comm)
 name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
+mdi.MDI_Send_Command("<ENERGY",comm)
+
+a = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, comm)
+
+latvecs = np.zeros(9)
+
+# Check for periodicity
+periodic = np.zeros(3, dtype=np.int32)
+mdi.MDI_Send_Command("<DIMENSIONS", comm)
+mdi.MDI_Recv(3, mdi.MDI_INT, comm, buf=periodic)
+
+# Lazy check
+if periodic[0] == 2:
+    mdi.MDI_Send_Command("<CELL", comm)
+    mdi.MDI_Recv(9, mdi.MDI_DOUBLE, comm, buf=latvecs)
+
+    # Retrieve the origin
+    origin = np.zeros(3)
+    mdi.MDI_Send_Command("<CELL_DISPL", comm)
+    mdi.MDI_Recv(3, mdi.MDI_DOUBLE, comm, buf=origin)
+
+    # Print
+    print("The simulation is periodic.")
+
+charges = np.zeros(3)
+mdi.MDI_Send_Command("<CHARGES", comm)
+mdi.MDI_Recv(3, mdi.MDI_DOUBLE, comm, buf=charges)
+
+coords = np.zeros(9)
+mdi.MDI_Send_Command("<COORDS", comm)
+mdi.MDI_Recv(9, mdi.MDI_DOUBLE, comm, buf=coords)
+
 
 # Print the name of the engine
 print("ENGINE NAME: " + str(name))
+print(f"ENERGY: {a}")
+print(f"CELL: {latvecs}")
+print(f"CELL_DISPL: {origin}")
+print(f"CHARGES: {charges}")
+print(f"COORDS: {coords}")
 
 mdi.MDI_Send_Command("EXIT", comm)
